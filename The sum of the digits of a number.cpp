bbd9c24b3f619413a1103b1af280f8fab5d7b93d
Ivan <ivankuznetsov454@yandex.ru>
2026-03-18 22:57:20 +0500

    The sum of the digits of a number

---
+++ The sum of the digits of a number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int var{}, sum{};
+	
+	std::cout << "¬ведите целое число:" << std::endl;
+	std::cin >> var;
+
+	while (var != 0) {
+		sum += var % 10;
+		var /= 10;
+	}
+	std::cout << "—умма: " << sum << std::endl;
+
+	return EXIT_SUCCESS;
+}
